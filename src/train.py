631d2637e1a0f9b5a9af1b96fb7c9048bf24db90
zainthemaynnn <zainyusufazam@gmail.com>
2021-08-21 19:55:30 -0400

    it works! also RIP project

--- src/train.py
+++ src/train.py
@@ -18,17 +18,25 @@ import nltk
 from transformers import AutoTokenizer, BertModel, BertConfig
 
 TEST_CUTOFF = 1000
+BATCH_SIZE = 128
+
+transformer = BertModel(BertConfig())
+
+def main():
+    train = torchtext.datasets.YelpReviewPolarity(
+        root=".data",
+        split="train"
+    )
+
+    samples, results = load(train)
+    samples = scrub(samples[:TEST_CUTOFF])
+
+    trainer = Trainer(Model(len(WORD2IDX), len(WORD2IDX) - 1))
+    trainer.run(50, get_dataloader(samples, results, TEST_CUTOFF, BATCH_SIZE))
+
 
 nltk.download("punkt")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-train, test = torchtext.datasets.YelpReviewPolarity(root = ".data", split = ("train", "test"))
-
-
-def tokenize(x):
-    """Tokenize samples."""
-    x = x.translate(str.maketrans(string.punctuation, " " * len(string.punctuation)))
-    return nltk.word_tokenize(x)
-
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 TOKENIZER = AutoTokenizer.from_pretrained("bert-base-uncased")
 WORD2IDX = TOKENIZER.vocab
@@ -37,9 +45,11 @@ PAD_TOKEN = TOKENIZER.pad_token_id
 UNK_TOKEN = TOKENIZER.unk_token_id
 MAX_LEN = 64
 
+def tokenize(x):
+    """Tokenize samples."""
+    x = x.translate(str.maketrans(string.punctuation, " " * len(string.punctuation)))
+    tokenized_x = nltk.word_tokenize(x)
 
-def encode(tokenized_x):
-    """Encode samples and pad."""
     new_sent = [] 
     for word in tokenized_x:
         word = word.lower()
@@ -52,7 +62,7 @@ def encode(tokenized_x):
     # Pad up to max length 
     padded_sent = [PAD_TOKEN] * MAX_LEN
     padded_sent[:len(new_sent)] = new_sent
-    return new_sent
+    return padded_sent
 
 
 def load(data):
@@ -72,7 +82,7 @@ def scrub(samples):
     """Tokenizes and encodes samples."""
     new_samples = []
     for sample in samples:
-        new_samples.append(encode(tokenize(sample)))
+        new_samples.append(tokenize(sample))
     return new_samples
 
 
@@ -106,13 +116,13 @@ class Model(nn.Module):
         self.hidden_dim = 16
         self.num_layers = 1
 
-        self.embedding_layer = nn.Embedding(self.vocab_len, self.embedding_dim, padding_idx = self.padding_idx)
-        self.rnn = nn.LSTM(self.embedding_dim, self.hidden_dim, self.num_layers, batch_first = True, bidirectional = True)
+        self.embedding_layer = nn.Embedding(self.vocab_len, self.embedding_dim, padding_idx=self.padding_idx)
+        self.RNN = nn.LSTM(self.embedding_dim, self.hidden_dim, self.num_layers, batch_first=True, bidirectional=True)
         self.classifier = nn.Linear(self.hidden_dim * 2 * self.num_layers, 1)
 
     def forward(self, x):
         embedding = self.embedding_layer(x)
-        _, (hidden, cell) = self.rnn(embedding)  
+        _, (hidden, cell) = self.RNN(embedding)  
 
         # hidden - The final state we use 
         # hidden: Tensor(Num layers, N, Dimensional) 
@@ -122,20 +132,20 @@ class Model(nn.Module):
         hidden = hidden.reshape(B, -1)
         # classifier head
         logits = self.classifier(hidden)
-        logits = logits.reshape(-1, )
+        logits = logits.reshape(-1,)
         return logits
 
 
 class Trainer:
     def __init__(self, model):
-        self.device = device
-        self.model = model.to(self.device)
+        self.DEVICE = DEVICE
+        self.model = model.to(self.DEVICE)
 
         self.optimizer = optim.Adam(self.model.parameters(), lr=5e-4)
         self.loss_function = nn.BCEWithLogitsLoss()
 
     def save(self):
-        torch.save(self.model.state_dict(), "data/model.pth")
+        torch.save(self.model.state_dict(), ".data/model.pth")
 
     def training_step(self, x, y):
         self.model.train()
@@ -144,7 +154,7 @@ class Trainer:
         self.optimizer.zero_grad()
 
         # forward pass
-        outputs = self.model.forward(x)
+        outputs = self.model(x)
 
         # loss function
         loss = self.loss_function(outputs, y)
@@ -156,8 +166,8 @@ class Trainer:
 
     def train(self, train_dataloader):
         for x, y in train_dataloader:
-            x = x.long().to(self.device)
-            y = y.float().to(self.device)
+            x = x.long().to(self.DEVICE)
+            y = y.float().to(self.DEVICE)
             self.training_step(x, y)
 
     def eval_step(self, x, y):
@@ -180,24 +190,23 @@ class Trainer:
         n = 0
 
         for x, y in eval_dataloader:
-            x = x.long().to(self.device)
-            y = y.float().to(self.device)
+            x = x.long().to(self.DEVICE)
+            y = y.float().to(self.DEVICE)
 
             loss, accuracy = self.eval_step(x, y)
             net_loss += loss
-            net_accuracy += net_accuracy
+            net_accuracy += accuracy
             n += 1
 
         net_loss /= n
         net_accuracy /= n
         print(f"Loss: {net_loss}; Accuracy: {net_accuracy}")
 
+    def run(self, num_epochs, train_dataloader, with_eval=None, eval_dataloader=None):
+        for epoch in range(num_epochs):
+            self.train(train_dataloader)
+            if with_eval:
+                self.eval(eval_dataloader)
 
-samples, results = load(train)
-samples = scrub(samples[:TEST_CUTOFF])
-trainer = Trainer(Model(len(WORD2IDX), len(WORD2IDX) - 1))
-trainer.train(get_dataloader(samples, results, TEST_CUTOFF, MAX_LEN))
-
-samples, results = load(test)
-samples = scrub(samples[:TEST_CUTOFF])
-trainer.eval(get_dataloader(samples, results, TEST_CUTOFF, MAX_LEN))
+if __name__ == "__main__":
+    main()
